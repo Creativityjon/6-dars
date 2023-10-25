e8105d2cf8994b938abd3d5c4acc61dc98c6b858
Muhammad Yusuf Hamidov <muhammadyusufhamidov@Muhammads-MacBook-Air.local>
2023-10-25 12:07:24 +0500

    sam

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let str = "Lorem ipsum dolor sit ipsum amet"
+
+// console.log(str.lenggs);
+// console.log(str.charAt(3));
+// console.log(str.charCodeAt(22));
+// console.log(str.concat("Eshmat", "Toshmat"));
+// console.log(str.includes("j"));
+// console.log(str.indexOf("a"));
+// console.log(str.lastIndexOf("a"));
+// console.log(str.padEnd(10, "0"));
+// console.log(str.padStart(10, "0"));
+// console.log(str.repeat(3));
+// console.log(str.replace("  ", "  "));
+// console.log(str.repeatAll(" ", ","))
+// console.log(str.search(/\+998*)); string and rexag
+// console.log(str.slice((14)));
+// console.log(str.split(" "));
+// console.log(str.startsWith("lorem"));
+// console.log(str.toLocaleLowerCase());
+// console.log(str.toLocaleUpperCase());
+// console.log(str.trim());
+// console.log(str.trimStart());
+// console.log(str.trimEnd());
